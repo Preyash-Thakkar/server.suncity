cb72a7581ce04469dff4eca65b0f0c0a60d3f0cf
Jay Mandaliya <bhailumandaliya11@gmail.com>
2024-01-09 14:03:15 +0530

    pull

--- routes/suncityAdmin.js
+++ routes/suncityAdmin.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const {stakeholderController} = require("../controllers")
+const {stakeholderController} = require("../controllers/stakeholderremarks")
 
 router.post('/add-remark', stakeholderController.addPlotRemark);
 
