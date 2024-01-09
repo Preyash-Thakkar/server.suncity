5da9ee60f1e3be114c0e9aa8865e4fecfcbe65dd
Jay Mandaliya <bhailumandaliya11@gmail.com>
2024-01-09 12:12:57 +0530

    final

--- models/plotdetail.js
+++ models/plotdetail.js
@@ -1,34 +1,34 @@
 const mongoose = require("mongoose");
 
-const Suncityplotdetails = new mongoose.Schema(
-{
-    plot_no: {
-      type: String,
-      required: true,
-      unique: true
-    },
-    status: {
-      type: String,
-      enum: ['available', 'sold', 'reserved'],
-      required: true
-    },
-    dimensions: {
-      length: {
-        type: Number,
-        required: true
-      },
-      width: {
-        type: Number,
-        required: true
-      }
-    },
-    area: {
+const Suncityplotdetails = new mongoose.Schema({
+  plot_no: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  status: {
+    type: String,
+    enum: ['available', 'sold', 'reserved', 'active'],
+    required: true
+  },
+  dimensions: {
+    length: {
       type: Number,
       required: true
     },
-    price: {
+    width: {
       type: Number,
       required: true
-    },
+    }
+  },
+  area: {
+    type: Number,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
 });
-  module.exports = mongoose.model("plotdetails",Suncityplotdetails );
+
+module.exports = mongoose.model("plotdetails", Suncityplotdetails);
