30ad5ac8919e9f021836b9e04d2965dd95fb6198
Dhruvit <pateldhruvit0712@gmail.com>
2024-01-12 16:00:15 +0530

    Inquiry List

--- routes/listinquiries.js
+++ routes/listinquiries.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { listInquiries } = require("../controllers/listinquiries");
 
-// Define route to handle form submission
 router.post("/listinquiries", listInquiries);
 
 module.exports = router;
