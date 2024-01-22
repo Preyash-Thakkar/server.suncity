e2c4547c958437836fbc4de312bdee3a9b7cab79
Jay Mandaliya <bhailumandaliya11@gmail.com>
2024-01-22 11:38:47 +0530

    done

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,9 +19,12 @@ const login = async (req, res) => {
 
     const token = jwt.sign({ userId: excecutive._id }, 'your-secret-key', { expiresIn: '1h' });
 
-    res.cookie('token', token, { httpOnly: true });
-
-    res.status(200).json({ message: 'Login successful', token });
+    res.cookie('token', token,'id passed',excecutive._id, { httpOnly: true });
+    console.log("execute name 1", excecutive._id);
+    console.log("execute name", excecutive.name);
+    const idstored = excecutive._id;
+    const executivename = excecutive.name;
+    res.status(200).json({ message: 'Login successful', token, idstored,executivename});
   } catch (error) {
     console.error('Login error:', error);
     res.status(500).json({ message: 'Internal Server Error' });
